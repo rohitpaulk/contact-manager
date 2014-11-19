3e1b2673f5fbddf677327261a31af3fdbf8c7e0e
Rohit Paul Kuruvilla <rohitpaul@iitrpr.ac.in>
2014-11-19 10:07:08 +0530

    added gravatar URL

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,10 @@
+require 'digest/md5'
+
 class Contact < ActiveRecord::Base
 	validates :name, presence: true
+
+	def avatar_url
+		hash = Digest::MD5.hexdigest(email.downcase)
+		"http://www.gravatar.com/avatar/#{hash}"
+	end
 end
